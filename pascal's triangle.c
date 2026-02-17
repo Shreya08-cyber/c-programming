5fe363054970e5018baf8245212cb3bbb5983d9d
Shreya08-cyber <192565018.simats@saveetha.com>
2026-02-17 17:16:51 +0530

    Create pascal's triangle.c

---
+++ pascal's triangle.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main() {
+    int n, i, j, coef;
+    printf("Enter number of rows: ");
+    scanf("%d", &n);
+    for(i = 0; i < n; i++) {
+    coef = 1;
+    for(j = 0; j <= i; j++) {
+    printf("%d ", coef);
+    coef = coef * (i - j) / (j + 1);
+        }
+        printf("\n");
+    }
+    return 0;
+}
