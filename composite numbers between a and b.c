80ec42151743e2b475618384a1414520c25aa4c4
Shreya08-cyber <192565018.simats@saveetha.com>
2026-02-17 16:58:34 +0530

    Create composite numbers between a and b.c

---
+++ composite numbers between a and b.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main() {
+    int a, b, i, j, count;
+    printf("Enter two numbers: ");
+    scanf("%d %d", &a, &b);
+    for(i = a; i <= b; i++) {
+        count = 0;
+        for(j = 1; j <= i; j++) {
+            if(i % j == 0)
+                count++;
+        }
+        if(count > 2)
+            printf("%d ", i);
+    }
+    return 0;
+}
