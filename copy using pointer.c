6812a13ae64e0aba04c2ace1ee8f231e3db61c09
Shreya08-cyber <192565018.simats@saveetha.com>
2026-03-01 22:13:50 +0530

    Create copy using pointer.c

---
+++ copy using pointer.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main() {
+    int arr1[5] = {10,20,30,40,50};
+    int arr2[5];
+    int *p1 = arr1, *p2 = arr2;
+    for(int i=0;i<5;i++) *(p2+i) = *(p1+i);
+    for(int i=0;i<5;i++) printf("%d ", arr2[i]);
+    return 0;
+}
