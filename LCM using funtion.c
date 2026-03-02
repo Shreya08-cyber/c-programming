6283f3ec66de927f46f366544c5325f937babe8a
Shreya08-cyber <192565018.simats@saveetha.com>
2026-03-02 21:31:47 +0530

    Create LCM using funtion.c

---
+++ LCM using funtion.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int gcd(int a, int b) {
+    while (b != 0) {
+        int temp = b;
+        b = a % b;
+        a = temp;
+    }
+    return a;
+}
+int lcm(int a, int b) {
+    return (a * b) / gcd(a, b);
+}
+int main() {
+    int x, y;
+    printf("Enter two numbers: ");
+    scanf("%d %d", &x, &y);
+    printf("LCM = %d\n", lcm(x, y));
+    return 0;
+}
