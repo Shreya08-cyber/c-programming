b013ff93b0267744527bf5e48cd2c3403d31a125
Shreya08-cyber <192565018.simats@saveetha.com>
2026-03-02 22:01:42 +0530

    Create sum of array using function.c

---
+++ sum of array using function.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int sumArray(int arr[], int n) {
+    int sum = 0;
+    for (int i = 0; i < n; i++) sum += arr[i];
+    return sum;
+}
+int main() {
+    int n;
+    printf("Enter size of array: ");
+    scanf("%d", &n);
+    int arr[n];
+    printf("Enter %d elements: ", n);
+    for (int i = 0; i < n; i++) scanf("%d", &arr[i]);
+    printf("Sum = %d\n", sumArray(arr, n));
+    return 0;
+}
