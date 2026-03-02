8989b4d553458623e4e87323b7fed9e0205acae5
Shreya08-cyber <192565018.simats@saveetha.com>
2026-03-02 22:03:47 +0530

    Create count vowels using functions.c

---
+++ count vowels using functions.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <ctype.h>
+
+int countVowels(char str[]) {
+    int count = 0;
+    for (int i = 0; str[i] != '\0'; i++) {
+        char ch = tolower(str[i]);
+        if (ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u')
+            count++;
+    }
+    return count;
+}
+
+int main() {
+    char str[100];
+    printf("Enter a string: ");
+    scanf("%s", str);
+    printf("Vowel Count = %d\n", countVowels(str));
+    return 0;
+}
